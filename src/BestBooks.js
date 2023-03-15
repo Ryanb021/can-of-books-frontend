34179caecd4369c0bfbea77b84ca16bfaa92e244
Ryan Bagan <darcknight1980@gmail.com>
2023-03-15 12:54:40 -0700

    made changes

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -33,104 +33,104 @@ class BestBooks extends React.Component {
 
   }
 
-deleteBooks = async (singleBook) => {
-  try {
-    let url = `${process.env.REACT_APP_SERVER}/books/${singleBook_id}`
+  deleteBooks = async (id) => {
+    try {
+      let url = `${process.env.REACT_APP_SERVER}/books/${id}`;
 
-    await axios.delete(url);
+      await axios.delete(url);
 
-    let updatedBooks = this.state.books.filter(book => book._id !== singleBook.id);
+      let updatedBooks = this.state.books.filter(book => book._id !== id);
 
-    this.setState({
-      books: updatedBooks
-    });
+      this.setState({
+        books: updatedBooks
+      });
 
-  } catch (error) {
-    console.log(error.message)
-  }
-}
-
-handleSyncBooks = (sync) => {
-  this.setState({
-    books: sync
-  }
-
-  )
-}
-componentDidMount() {
-  this.getBooks();
-}
-
-handleBook = () => {
-  this.props.handleOpenModal();
-}
-
-
-render() {
-  /* TODO: render all the books in a Carousel */
-  let booksCarousel = this.state.books.map(
-    i => {
-      return (
-        <Carousel.item key={i._id}>
-          <img
-            src=''
-            alt={i.title}
-          />
-          <Carousel.Caption>
-            <h3>{i.title}</h3>
-            <p>{i.description}</p>
-            <p>Status: {i.status ? 'Available' : 'Not Available'}</p>
-            <Button type='submit' variant="outline-dark" onClick={() =>
-              this.deleteBooks(i_id)}>Delete This Book</Button>
-
-          </Carousel.Caption>
-
-        </Carousel.item>
-      )
+    } catch (error) {
+      console.log(error.message)
     }
-  );
+  }
 
-  return (
-    <>
-      <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
+  handleSyncBooks = (sync) => {
+    this.setState({
+      books: sync
+    }
 
-      // {this.state.books.length ? (
-<Carousel fade variant="dark">
-  {booksCarousel}
-</Carousel>
-        //   <Carousel>
-        //     {this.state.books.map((book, idx) => {
-        //       return (
+    )
+  }
+  componentDidMount() {
+    this.getBooks();
+  }
 
-        //         <Carousel.Item key={idx}>
-        //           <Button onClick={() => { }}>Delete</Button>
-        //           <img
-
-        //           />
-        //           <Carousel.Caption>
-        //             <h3>{book.title}</h3>
-        //             <p></p>
-        //           </Carousel.Caption>
-        //         </Carousel.Item>
-        //       )
-        //     })}
+  handleBook = () => {
+    this.props.handleOpenModal();
+  }
 
 
-        //   </Carousel>
-      ): (
+  render() {
+    /* TODO: render all the books in a Carousel */
+    let booksCarousel = this.state.books.map(
+      i => {
+        return (
+          <Carousel.Item key={i._id}>
+            <img
+              src=''
+              alt={i.title}
+            />
+            <Carousel.Caption>
+              <h3>{i.title}</h3>
+              <p>{i.description}</p>
+              <p>Status: {i.status ? 'Available' : 'Not Available'}</p>
+              <Button type='submit' variant="outline-dark" onClick={() =>
+                this.deleteBooks(i._id)}>Delete This Book</Button>
+
+            </Carousel.Caption>
+
+          </Carousel.Item>
+        )
+      }
+    );
+
+    return (
+      <>
+        <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
+
+        {this.state.books.length ? (
+          <Carousel fade variant="dark">
+            {booksCarousel}
+          </Carousel>
+          //   <Carousel>
+          //     {this.state.books.map((book, idx) => {
+          //       return (
+
+          //         <Carousel.Item key={idx}>
+          //           <Button onClick={() => { }}>Delete</Button>
+          //           <img
+
+          //           />
+          //           <Carousel.Caption>
+          //             <h3>{book.title}</h3>
+          //             <p></p>
+          //           </Carousel.Caption>
+          //         </Carousel.Item>
+          //       )
+          //     })}
+
+
+          //   </Carousel>
+        ) : (
           <h3>No Books Found :</h3>
-  )}
+        )}
 
-      <Button onClick={this.handleBook}>Add Book</Button>
-      <BookFormModal
-        handleCloseModal={this.props.handleCloseModal}
-        showModal={this.state.books}
-        handleSyncBooks={this.handleSyncBooks}
-      />
-    </>
-  
-  )
-}
+        <Button onClick={this.handleBook}>Add Book</Button>
+        <BookFormModal
+          handleCloseModal={this.props.handleCloseModal}
+          showModal={this.state.books}
+          handleSyncBooks={this.handleSyncBooks}
+        />
+      </>
+
+    )
+  }
 }
 
 
