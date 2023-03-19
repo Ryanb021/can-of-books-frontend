33c3161c009f2a7f3171566c6f1bfbba875c14a1
Ryan Bagan <darcknight1980@gmail.com>
2023-03-19 00:57:19 -0700

    made changes

--- src/Content.js
+++ src/Content.js
@@ -1,7 +1,7 @@
 import { withAuth0 } from "@auth0/auth0-react";
 import React from 'react';
 import BestBooks from './BestBooks';
-import Welcome from './Welcome';
+import './Content.css';
 
 class Content extends React.Component {
 
@@ -9,12 +9,13 @@ class Content extends React.Component {
     console.log('it is working');
     return (
       <>
-      {this.props.auth0.isAuthenticated ?
+        {this.props.auth0.isAuthenticated ?
 
-        <BestBooks />
-        :
+          <BestBooks />
+          :
+          <p className="why">What are you here for? <br />You need to Log In!</p>
 
-        <Welcome />}
+          }
       </>
     )
   }
