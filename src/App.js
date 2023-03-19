252c5752fad2fa291bceb66921940cc8a077a6c8
Ryan Bagan <darcknight1980@gmail.com>
2023-03-19 00:44:53 -0700

    made changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
-import BestBooks from './BestBooks';
+//import BestBooks from './BestBooks';
 import About from './About';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-
+//import LoginButton from './LoginButton';
+//import LogoutButton from './LogOutButton';
+import Profile from './Profile';
+import Content from './Content';
+//import { withAuth0 } from "@auth0/auth0-react";
 
 import {
   BrowserRouter as Router,
@@ -17,23 +21,35 @@ class App extends React.Component {
   render() {
     return (
       <>
-        <Router>
+          <Router>
           <Header />
           <Routes>
             <Route
               exact path="/"
-              element={<BestBooks
-              />}
+              element={
+                <Content />
+              
+              }
             >
             </Route>
-            <Route exact path="/about"
+
+            
+            <Route
+              exact path="/about"
               element={<About />}
             >
-
+            </Route>
+            <Route
+              exact path="/profile"
+              element={
+                <Profile />
+            }
+            >
             </Route>
           </Routes>
           <Footer />
         </Router>
+
       </>
     )
   }
